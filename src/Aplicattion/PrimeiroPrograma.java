4b42b810b394ddcde6b1eb2db82b3ef9908c1a25
ACLECIO PEREIRA DOS SANTOS <aclecio11@gmail.com>
2021-09-04 21:30:09 -0300

    primeiro commit

---
+++ src/Aplicattion/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package Aplicattion;
+
+import Aplicattion.entities.Carro;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(carro);
+
+    }
+}
---
+++ src/Aplicattion/entities/Carro.java
@@ -0,0 +1,75 @@
+package Aplicattion.entities;
+
+import java.util.Objects;
+
+public class Carro {
+    private String modelo;
+    private String marca;
+    private String cor;
+    private int ano;
+
+    public Carro() {
+    }
+
+    public Carro(String modelo, String marca, String cor, int ano) {
+        this.modelo = modelo;
+        this.marca = marca;
+        this.cor = cor;
+        this.ano = ano;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Carro carro = (Carro) o;
+        return ano == carro.ano && Objects.equals(modelo, carro.modelo) && Objects.equals(marca, carro.marca) && Objects.equals(cor, carro.cor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(modelo, marca, cor, ano);
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "modelo='" + modelo + '\'' +
+                ", marca='" + marca + '\'' +
+                ", cor='" + cor + '\'' +
+                ", ano=" + ano +
+                '}';
+    }
+}
